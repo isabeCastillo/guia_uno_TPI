55b279c0dbb169fb5819aa110c8c8a7e833aedb5
isabeCastillo <cp21026@ues.edu.sv>
2025-09-03 19:11:00 -0600

    feat: agregando campos de telefono y codigo postal que solo acepten numero

--- ejerciciouno/EjerValidacion/ejerciUno.php
+++ ejerciciouno/EjerValidacion/ejerciUno.php
@@ -4,11 +4,18 @@ $mensaje = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = trim($_POST["nombre"]);
     $ciudad = trim($_POST["ciudad"]);
+    $telefono = trim($_POST["telefono"]);
+    $codigoPostal = trim($_POST["codigoPostal"]);
+
     if (preg_match('/[0-9]/', $nombre)) {
         $mensaje = "El campo  Nombre completo no puede contener numeros.";
     } elseif (preg_match('/[0-9]/', $ciudad)) {
         $mensaje = "El campo ciudad no puede contener numeros";
-    } else{
+    } elseif (!ctype_digit($telefono)) {
+        $mensaje = "El campo telefono no puede contener letras";
+    } elseif (!ctype_digit($codigoPostal)) {
+        $mensaje = "El campo codigo postal no puede contener letras";
+    }else{
         $mensaje = "Datos correctos!";
     }
 }
@@ -25,11 +32,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <form action="" method="post">
         <input type="text" name="nombre" id="" placeholder="Nombre completo">
         <input type="text" name="ciudad" id="" placeholder="Ciudad">
+        <input type="text" name="telefono" id="" placeholder="Telefono">
+        <input type="text" name="codigoPostal" id="" placeholder="Codigo postal">
         <button type="submit">Procesando datos</button>
     </form>
 
     <?php if ($mensaje): ?>
-        <p class="<?= strpos($mensaje, '❌') !== false ? 'error' : 'ok' ?>">
+        <p class="<?= $mensaje === 'Datos correctos!' ? 'ok' : 'error' ?>">
             <?= $mensaje ?>
         </p>
     <?php endif; ?>
