1424d52c43feb5c9cc31af98a5e479257526e613
isabeCastillo <cp21026@ues.edu.sv>
2025-09-03 19:42:00 -0600

    feat: haciendo validacion para que el numero de telefono no seamas de 9, haciendo que el codigo postal sea 5 los digitos ingresados y que el codigo postal coincida con los primero dos digitos de la provincia

--- ejerciciouno/EjerValidacion/ejerciUno.php
+++ ejerciciouno/EjerValidacion/ejerciUno.php
@@ -2,20 +2,23 @@
 $mensaje = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = trim($_POST["nombre"]);
-    $ciudad = trim($_POST["ciudad"]);
-    $telefono = trim($_POST["telefono"]);
+    $nombre       = trim($_POST["nombre"]);
+    $ciudad       = trim($_POST["ciudad"]);
+    $telefono     = trim($_POST["telefono"]);
     $codigoPostal = trim($_POST["codigoPostal"]);
+    $provincia    = trim($_POST["provincia"]);
 
     if (preg_match('/[0-9]/', $nombre)) {
         $mensaje = "El campo  Nombre completo no puede contener numeros.";
     } elseif (preg_match('/[0-9]/', $ciudad)) {
         $mensaje = "El campo ciudad no puede contener numeros";
-    } elseif (!ctype_digit($telefono)) {
-        $mensaje = "El campo telefono no puede contener letras";
-    } elseif (!ctype_digit($codigoPostal)) {
-        $mensaje = "El campo codigo postal no puede contener letras";
-    }else{
+    } elseif (!ctype_digit($telefono) > 9) {
+        $mensaje = "El campo telefono debe contener solo números y no más de 9 dígitos";
+    } elseif (!ctype_digit($codigoPostal) || strlen($codigoPostal) !== 5) {
+        $mensaje = "El campo codigo postal debe contener exactamente 5 números";
+    } elseif (substr($provincia, 0, 2) !== substr($codigoPostal, 0, 2)) {
+        $mensaje = "El código postal no coincide con el departamento seleccionado.";
+    } else{
         $mensaje = "Datos correctos!";
     }
 }
@@ -34,6 +37,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <input type="text" name="ciudad" id="" placeholder="Ciudad">
         <input type="text" name="telefono" id="" placeholder="Telefono">
         <input type="text" name="codigoPostal" id="" placeholder="Codigo postal">
+        <label for="provincia">Departamento de oriente</label>
+
+        <select name="provincia" required>
+            <option value="">Selecciona un Departamento</option>
+            <option value="31-launion">31 - La Unión</option>
+            <option value="32-morazan">32 - Morazán</option>
+            <option value="33-sanmiguel">33 - San Miguel</option>
+            <option value="34-usulutan">34 - Usulután</option>
+        </select>
+
         <button type="submit">Procesando datos</button>
     </form>
 
